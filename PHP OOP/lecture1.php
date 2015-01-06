8a2f12bfcdad10d2336918e75883f25f4ffc6db4
EleanorStrotz <EleanorStrotz@gmail.com>
2015-01-06 11:26:54 -0800

    commit 1

---
+++ PHP OOP/lecture1.php
@@ -0,0 +1,37 @@
+<?php
+// Example 1
+class bird{
+	//text
+}
+class cat{
+	//text
+}
+class mouse{
+	//text
+}
+
+//Example 2
+$bird1 = new bird();
+$bird2 = new bird();
+
+$cat1 = new cat();
+$cat2 = new cat();
+
+$mouse1 = new mouse();
+$mouse2 = new mouse();
+
+//Example 3
+class bird {
+	public $orgin = "default name";
+	public $breed = "best breed";
+	public $gender = "female";
+	public $price = 0;
+
+}
+class cat {
+
+}
+class mouse{
+	
+}
+?>
