cf8b0463876ddf754f6ab990982fc85d117cea92
EleanorStrotz <EleanorStrotz@gmail.com>
2015-01-08 11:09:14 -0800

    run commit

--- PHP OOP/run.php
+++ PHP OOP/run.php
@@ -1,5 +1,54 @@
 <?php
-// example 1
+// example 1 is_bool()
+$a = false;
+$b = 0;
+
+// Since $a is a boolean, it will return true
+if (is_bool($a) === true) {
+    echo "Yes, this is a boolean";
+}
+
+// Since $b is not a boolean, it will return false
+if (is_bool($b) === false) {
+    echo "No, this is not a boolean";
+}
+
+// __________________________________________________________________________________________________________________________________
+
+ // example 2 is_object()
+// Declare a simple function to return an 
+// array from our object
+function get_students($obj)
+{
+    if (!is_object($obj)) {
+        return false;
+    }
+
+    return $obj->students;
+}
+
+// Declare a new class instance and fill up 
+// some values
+$obj = new stdClass();
+$obj->students = array('Kalle', 'Ross', 'Felipe');
+
+var_dump(get_students(null));
+var_dump(get_students($obj));
+
+
+// __________________________________________________________________________________________________________________________________
+
+ // example 3 is_array() 
+
+$yes = array('this', 'is', 'an array');
+
+echo is_array($yes) ? 'Array' : 'not an Array';
+echo "\n";
+
+$no = 'this is a string';
+
+echo is_array($no) ? 'Array' : 'not an Array';
+
 
 
 
