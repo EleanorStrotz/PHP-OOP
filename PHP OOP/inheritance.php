e5d298eb9473518eb04ef139c269bba7d25e3b15
EleanorStrotz <EleanorStrotz@gmail.com>
2015-01-13 11:29:50 -0800

    b

--- PHP OOP/inheritance.php
+++ PHP OOP/inheritance.php
@@ -1,22 +1,38 @@
 <?php
-class person {
+// example 1
+class Person 
+{
 	public $firstname;
 	public $lastname;
 	public $gender;
 
-	fucntion __construct($firstname, $lastname, $gender){
+	function __construct($firstname, $lastname, $gender)
+	{
 		$this->firstname = $firstname;
 		$this->lastname = $lastname;
 		$this->gender = $gender;
 	}
 
-	function greeting(){
+	function greeting()
+	{
 		return "Hello my name is " . $this->firstname . 
 		$this->lastname . "I am a " . $this->gender;
 	}
 }
-class Eleanor extends person{
-	function getName() 
-}
 
+
+$person1 = new Person("eleanor", "strotz","female");
+echo $person1->greeting();
 ?>
+
+
+/*
+class Eleanor extends person
+{ 
+	public $eyecolor;
+
+	public function __construct($firstname, $lastname, $gender, $eyecolor)
+	{
+		super($firstname, $lastname, $gender)
+		$this->eyecolor = $eyecolor;
+	} */
